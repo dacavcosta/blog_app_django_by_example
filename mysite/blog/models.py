ac7f9d89c22e5d6446ca01014b645bded7a7543f
dacavcosta <dcavalcanticosta@gmail.com>
2021-11-24 22:14:49 -0300

    Created the url paths for the Post Views Added the blog app path to the mysite path Created the Canonical URL for the post_detail view

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.utils import timezone
 
 class PublishedManager(models.Manager):
@@ -42,3 +43,10 @@ class Post(models.Model):
         
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
---
+++ mysite/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    #Post Views
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_list, name='post_detali'),
+]
