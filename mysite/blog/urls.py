ac261a977274e4a56d4f1753d16103db5eee80bb
dacavcosta <dcavalcanticosta@gmail.com>
2021-12-13 20:02:36 -0300

    Created the templates to the blog app base.html detail.html list.html Fixed the route to post_detail

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,5 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     #Post Views
     path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_list, name='post_detali'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>',
+         views.post_detail,
+         name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,12 +8,11 @@ def post_list(request):
                   {'posts': posts})
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             slug=post,
-                             status='published',
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
+    post = get_object_or_404(Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
     return render(request,
                   'blog/post/detail.html',
-                  {'post':post})
+                  {'post': post})
