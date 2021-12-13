3b1996911e6447a84732d97e6f02664ebec80a73
dacavcosta <dcavalcanticosta@gmail.com>
2021-12-13 20:23:34 -0300

    Created the pagination.html template Added pagination to list.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,20 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page':page, 'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
