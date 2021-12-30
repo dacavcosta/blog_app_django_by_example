dc0a74b9f53919a44adfb65cf145feb59aa087c4
dacavcosta <dcavalcanticosta@gmail.com>
2021-12-30 13:09:50 -0300

    Created feed.py file  Added LatestPostsFeed class with items, item_title and item_description  methods Added the feed path to urls.py Added link to subscribe to feed in base.html

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -12,4 +14,5 @@ urlpatterns = [
          views.post_detail,
          name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
